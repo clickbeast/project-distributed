ebb051e3c39ae6b7ae540296087fd46a4eeb70db
simonvermeir <clickbeast@gmail.com>
2019-11-12 16:06:59 +0100

    fix: temp running version

--- Client/src/main/java/Main.java
+++ Client/src/main/java/Main.java
@@ -1,8 +1,12 @@
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -12,49 +16,88 @@ public class Main extends Application {
     // keep a reference to the main window controller
     private MainWindowViewController mainWindowViewController;
 
+//    public void start(Stage primaryStage) throws Exception {
+//
+//        // region UI SETUP
+//
+//        this.clientManager = new ClientManager();
+//
+//        FXMLLoader loader = new FXMLLoader(getClass().getResource("main.fxml"));
+//
+//        // provide the controller with a reference of the simulationManager
+//        loader.setControllerFactory(c -> {
+//            if (c == MainWindowViewController.class) {
+//                MainWindowViewController mc = new MainWindowViewController();
+//                mc.setClientManager(this.clientManager);
+//                mainWindowViewController = mc;
+//                return mc;
+//            } else {
+//                try {
+//                    return c.newInstance();
+//                } catch (Exception exc) {
+//                    throw new RuntimeException(exc);
+//                }
+//            }
+//        });
+//
+//        Parent flowPane = loader.load();
+//
+//        primaryStage.setTitle("Linkonardo Da Criptni");
+//        // primaryStage.setResizable(false);
+//        primaryStage.setScene(new Scene(flowPane, 1200, 860));
+//        primaryStage.setMinWidth(800);
+//        primaryStage.setMinHeight(750);
+//        primaryStage.show();
+//
+//        this.clientManager.setMainWindowViewController(mainWindowViewController);
+//        mainWindowViewController.scene = primaryStage.getScene();
+//
+//        // endregion
+//
+//        /**
+//         *
+//         * RUN YOUR TESTS BELOW THIS
+//         */
+//
+//
+//    }
+
+
     @Override
     public void start(Stage primaryStage) throws Exception {
 
-        // region UI SETUP
 
-        this.clientManager = new ClientManager();
 
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("main.fxml"));
+
+
+        Parent root = FXMLLoader.load(getClass().getResource("layout.fxml"));
+
 
         // provide the controller with a reference of the simulationManager
-        loader.setControllerFactory(c -> {
-            if (c == MainWindowViewController.class) {
-                MainWindowViewController mc = new MainWindowViewController();
-                mc.setClientManager(this.clientManager);
-                mainWindowViewController = mc;
-                return mc;
-            } else {
-                try {
-                    return c.newInstance();
-                } catch (Exception exc) {
-                    throw new RuntimeException(exc);
-                }
-            }
-        });
+//        loader.setControllerFactory(c -> {
+//            if (c == MainWindowViewController.class) {
+//                MainWindowViewController mc = new MainWindowViewController();
+//                mc.setClientManager(this.clientManager);
+//                mainWindowViewController = mc;
+//                return mc;
+//            } else {
+//                try {
+//                    return c.newInstance();
+//                } catch (Exception exc) {
+//                    throw new RuntimeException(exc);
+//                }
+//            }
+//        });
 
-        Parent flowPane = loader.load();
 
-        primaryStage.setTitle("Linkonardo Da Criptni");
-        // primaryStage.setResizable(false);
-        primaryStage.setScene(new Scene(flowPane, 1200, 860));
-        primaryStage.setMinWidth(800);
-        primaryStage.setMinHeight(750);
+
+
+
+        Scene scene = new Scene(root, 300, 275);
+
+        primaryStage.setTitle("Da Cripti");
+        primaryStage.setScene(scene);
         primaryStage.show();
-
-        this.clientManager.setMainWindowViewController(mainWindowViewController);
-        mainWindowViewController.scene = primaryStage.getScene();
-
-        // endregion
-
-        /**
-         *
-         * RUN YOUR TESTS BELOW THIS
-         */
 
     }
 
