023bc47c897f7b2cf28123b60a689c99f99b20bb
Unknown <arne.de.geeter0@gmail.com>
2019-12-10 14:09:29 +0100

    simpleClient Setup

--- SimpleClient/src/main/java/Main.java
+++ SimpleClient/src/main/java/Main.java
@@ -1,6 +1,12 @@
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
 
 public class Main {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws RemoteException, NotBoundException, MalformedURLException {
+        Chat stub = (Chat) Naming.lookup("rmi://localhost:5000/sonoo");
+        System.out.println(stub.sendMessage(0, "abc", "123"));
+        System.out.println(stub.getMessage(0, "abc"));
     }
 }
